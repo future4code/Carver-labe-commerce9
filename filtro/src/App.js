324d2847e66545ec3228c1e1d306bfd30f2d35e9
CarlosToledoMartins <guto_highlander@hotmail.com>
2021-11-10 17:12:00 -0300

    filtro

---
+++ filtro/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Filtro from './components/Filtro'
+
+function App() {
+  return (
+    <div>
+    <Filtro/>
+     
+    </div>
+  );
+}
+
+export default App
